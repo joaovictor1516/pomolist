aa48c33d977817f1b4947d04ba7d1400cb6582d3
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-03-27 11:13:40 -0300

    criacao do componente responsavel pela todo-list e do componente responsavel por tornar a mensagem da ab dinamica

---
+++ src/app/components/dinamicTab.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export function DinamicTab(){
+    return(
+        <div>
+        </div>
+    )
+}
---
+++ src/app/components/todo_list.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export function TodoList(){
+    return(
+        <div className="">
+            
+        </div>
+    )
+}
