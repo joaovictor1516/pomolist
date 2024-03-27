efcb56958753400c122fbdb71025ccefa6052317
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-03-27 10:53:47 -0300

    adicao dos components na pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,19 @@
 import { useState, useEffect } from "react";
 import { Clock } from "./components/clock";
 import { Header } from "./components/header";
+import { DinamicTab } from "./components/dinamicTab";
+import { Task } from "./components/tasks";
+import { TodoList } from "./components/todo_list";
 import "tailwindcss/tailwind.css";
 
 export function Principal(){
   return(
       <div className="">
+          <DinamicTab/>
           <Header/>
           <Clock/>
+          <Task/>
+          <TodoList/>
       </div>
   )
 }
