b6446794c927d47256c2c872a9832f1bdd2346c4
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-03-28 14:48:40 -0300

    importando as bibliotecas e inicio do dessenvolvimento do componente

--- src/app/components/todo_list.tsx
+++ src/app/components/todo_list.tsx
@@ -1,9 +1,33 @@
 "use client";
+import { useEffect, useState } from "react";
+import { Task } from "./tasks";
+import { X } from "lucide-react";
+import * as Dialog from "@radix-ui/react-dialog";
+import "tailwindcss/tailwind.css";
 
 export function TodoList(){
     return(
-        <div className="">
-            
-        </div>
+        <Dialog.Root>
+            <Dialog.Trigger className="">
+                <div className="">
+
+                </div>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+                <Dialog.Overlay className=""/>
+
+                <Dialog.Content className="">
+                    
+                    <Dialog.Close className="">
+                        <X className=""/>
+                    </Dialog.Close>
+
+                    <div className="">
+                        
+                    </div>
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
     )
 }
