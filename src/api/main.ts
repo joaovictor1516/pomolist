cfe82e5989d955b48d024c26d886e175e1e61d50
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-04-02 13:50:08 -0300

    desenvolvimento das primeiras funcoes das classes Task e TodoList da api

--- src/api/main.ts
+++ src/api/main.ts
@@ -1,29 +1,51 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { TodoListElement } from "../interfaces/interfaces";
+import { TodoListElement, TaskElement } from "../interfaces/interfaces";
 
 export class Task{
-    name: string;
+    id: string;
+    title: string;
     content: string;
+    completed: boolean;
+    todoListMember: boolean;
+    todoListPropriets: TodoListElement[];
+    
 
-    constructor(name: string, content: string){
-        this.name = name;
+    constructor(id: string, title: string, content: string, completed: boolean, todoListMember: boolean, todoListPropriets: TodoListElement[]){
+        this.id = id;
+        this.title = title;
         this.content = content;
+        this.completed = completed;
+        this.todoListMember = todoListMember;
+        this.todoListPropriets = todoListPropriets;
     }
 
-    creatTask(){
-        console.log(this.name, this.content)
-    }
+    creatTask(): TaskElement{
+        const task: TaskElement = {
+            id: this.id,
+            title: this.title,
+            content: this.content,
+            completed: this.completed,
+            todoListMember: this.todoListMember,
+            todoListPropriets: this.todoListPropriets
+        };
+
+        return task;
+    };
 };
 
 export class TodoList extends Task{
-    list: string[];
+    list: TodoListElement[];
+    idTodoList: string;
 
-    constructor(list: string[], name: string, content: string){
-        super(name, content);
+    constructor(idTodoList: string, list: TodoListElement[], id: string, title: string, content: string, completed: boolean, todoListMember: boolean, todoListPropriets: TodoListElement[]){
+        super(id, title, content, completed, todoListMember, todoListPropriets);
         this.list = list;
+        this.idTodoList = idTodoList;
+        const task = new Task(id, title, content, completed, todoListMember, todoListPropriets);
+        this.addTask(task);
     }
 
-    addTask(){
-        this.list.push();
+    addTask(task: TaskElement){
+        this.list.push(task);
     }
 }
