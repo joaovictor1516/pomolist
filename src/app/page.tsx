b55e68d313c6a2caf0c58dd4bc822edb1f57b69c
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-05-08 18:56:38 -0300

    criacao da lista encadeada que ira conter as tesks. ela serve para organiza-las e manupular a ordem das tesks, tabem sera emplementada na todo-list mas de forma diferente.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,40 @@ import { DinamicTab } from "./components/dinamicTab";
 import { Task } from "./components/tasks";
 import { TodoList } from "./components/todo_list";
 import {  TaskApi, TodoListApi } from "../api/main";
+import { TaskElement } from "../interfaces/interfaces";
 
 export default function Principal(){
   const [taskGroup, setTaskGroup] = useState();
+
+  class Node<TaskElement>{
+    data: TaskElement;
+    next: TaskElement | null;
+    constructor(data: TaskElement){
+      this.data = data;
+      this.next = null
+    };
+  };
+
+  class LinkedList{
+    head: TaskElement | null;
+    constructor(){
+      this.head = null;
+    };
+
+    increment(data: TaskElement){
+      const newNode = new Node(data);
+
+      if(!this.head){
+        this.head = newNode;
+      } else{
+        let current = this.head;
+        while(current.next){
+          current = current.nextç
+        };
+        current.next = newNode;
+      };
+    }
+  }
 
   return(
       <div className="">
