7a5a7ce42bf5f52b2d916141e2e0c74b657c4ba1
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-05-21 22:04:25 -0300

    adicao da logica do Dialog.Trigger.

--- src/app/components/task/tasks.tsx
+++ src/app/components/task/tasks.tsx
@@ -1,22 +1,31 @@
 "use client";
-import { ChangeEvent, useEffect, useState } from "react";
+import { useState } from "react";
 import { X } from "lucide-react";
 import * as Dialog from "@radix-ui/react-dialog";
 import "tailwindcss/tailwind.css";
 import { TaskElement } from "@/src/interfaces/interfaces";
 
-export function Task(){
+export function Task(props: TaskElement){
     const [task, setTask] = useState<TaskElement[]>([]);
 
-    const focusTime = () => {
-
+    const focusTime = (time: Date) => {
+        const taskTime = new Date();
+        taskTime.setMinutes(0);
+        if(time.getMinutes() === taskTime.getMinutes()){}
     };
 
     return(
         <Dialog.Root>
             <Dialog.Trigger>
                 <div className="">
-                    teste
+                    <div className="">
+                        <span className="">{props.title}</span>
+                        <p className="">{props.content}</p>                        
+                    </div>
+                    <div className="">
+                        <p className="">Tempo da atividade:</p>
+                        <p>{props.timeTask}</p>
+                    </div>
                 </div>
             </Dialog.Trigger>
 
@@ -34,6 +43,7 @@ export function Task(){
                             <p>Title:</p>
                             <input type="text" id="taskTitle" required className=""/>
                             <textarea name="taskContent" id="taskContent" required className=""/>
+                            <input type="button" value="Editar"/>
                             <input type="button" value="Deletar" />
                         </form>
                     </div>
