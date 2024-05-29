fcd41cd4e59f5df3026bd2a4900fc51a3626b8c6
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-05-29 11:47:42 -0300

    criacao da funcao takeTaskTime, responsavel por pegar os timers das tasks e cronometrar.

--- src/app/components/clock.tsx
+++ src/app/components/clock.tsx
@@ -1,9 +1,21 @@
 "use client";
 import { useState } from "react";
 import "tailwindcss/tailwind.css";
+import { TaskElement, TaskClock } from "@/src/interfaces/interfaces";
 
 export function Clock(){
+    const [tasks, setTasks] = useState<TaskElement[]>([]);
+    const [worksTime, setWorksTime] = useState<Date>();
+
+    function takeTaskTime(id: string){
+        const job: TaskElement[] = tasks.filter((task) => task.id === id);
+        setWorksTime(job[0].timeTask);
+
+    }
+
     return(
-        <div className=""></div>
+        <div className="">
+
+        </div>
     )
 }
