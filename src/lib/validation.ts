73fad3a7a853082933cc39d4e6a83159257a9303
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-06-24 19:08:44 -0300

    criacao da funcao de verificacao da tipagem dos dados da api da task.

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,5 +1,10 @@
 import { z } from "zod";
 
 export const taskSchema = z.object({
-    
-})
+    id: z.string().uuid(),
+    title: z.string().min(1, "Digite o titulo da tarefa."),
+    content: z.string().min(5, "Digite o conteudo da tarefa."),
+    timeTask: z.date(),
+    longTimeTask: z.date(),
+    shortTimeTask: z.date()
+});
