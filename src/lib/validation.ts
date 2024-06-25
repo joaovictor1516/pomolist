783b49a45a4b8a0aa06d2c9712c23bea88e09fb6
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-06-25 18:15:48 -0300

    criacao da funcao userSchema.

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -6,5 +6,13 @@ export const taskSchema = z.object({
     content: z.string().min(5, "Digite o conteudo da tarefa."),
     timeTask: z.date(),
     longTimeTask: z.date(),
-    shortTimeTask: z.date()
+    shortTimeTask: z.date(),
+    completed: z.boolean()
+});
+
+export const userSchema = z.object({
+    id: z.string().uuid(),
+    email: z.string().email(),
+    password: z.string(),
+    
 });
