4cdecb1d67e450f40557564a35c44c02cda1b9a1
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-06-26 15:59:39 -0300

    conclusao da userSchema e criacao, e conclusao, do todoListSchema.

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const taskSchema = z.object({
-    id: z.string().uuid(),
+    id: z.number(),
     title: z.string().min(1, "Digite o titulo da tarefa."),
     content: z.string().min(5, "Digite o conteudo da tarefa."),
     timeTask: z.date(),
@@ -10,9 +10,17 @@ export const taskSchema = z.object({
     completed: z.boolean()
 });
 
+export const todoListSchema = z.object({
+    id: z.number(),
+    completed: z.boolean(),
+    tasks: z.array(taskSchema)
+});
+
 export const userSchema = z.object({
     id: z.string().uuid(),
     email: z.string().email(),
     password: z.string(),
-    
+    taskMaked: z.boolean(),
+    tasks: z.array(taskSchema).nullable(),
+    todoLists: z.array(todoListSchema).nullable()
 });
