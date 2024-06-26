5ff11cff52f132eedaaa9acd3d6a00d8f8a3516c
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-06-26 11:24:13 -0300

    criacao do component user. este componente tem como responsabilidade a logica de funcionamento da pagina do usuario e das funcionalidades do usuario.

---
+++ src/app/components/user.tsx
@@ -0,0 +1,11 @@
+"use client";
+import axios from "axios";
+import "tailwindcss/tailwind.css";
+import { useState } from "react";
+import { X, UserCircle2 } from "lucide-react";
+
+import { UserElement } from "@/src/interfaces/interfaces";
+
+export function User(props: UserElement){
+    
+}
