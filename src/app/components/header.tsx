cce37a3478f80a2c23a59bbe2bba843b5eeee08e
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-06-28 18:44:12 -0300

    importacao de mais materiais para o componente, criacao das funcoes que irao receber se o usuario ira logar ou criar uma conta.

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,8 +1,25 @@
 "use client";
 import { useState } from "react";
 import "tailwindcss/tailwind.css";
+import { UserLogin } from "./user/userLogin";
+import { UserSignIn } from "./user/userSignIn";
+import { LogIn, User2, Settings } from "lucide-react";
 
 export function Header(){
+    const [userLogin, setUserLogin] = useState<boolean>(false);
+    
+    function isUserSignIn(){
+        if(userLogin){
+            setUserLogin(false);
+        }
+    }
+
+    function isUserLogin(){
+        if(!userLogin){
+            setUserLogin(true);
+        }
+    }
+
     return(
         <div className="">
             
