a681f523d6c7584547db81373ca996c07c4d544e
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-07-01 00:22:41 -0300

    criacao e inicio do desenvolvimento do middleware. ele tem como importancia a autenticacao do usuario.

---
+++ src/lib/middleware.ts
@@ -0,0 +1,8 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import jwt from "jsonwebtoken";
+
+const secretKey = process.env.KEY_JWT;
+
+export function authentication(){
+    
+}
