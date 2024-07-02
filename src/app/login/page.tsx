b07c563aa522ce36380efa444131045ca0b00e63
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-07-02 12:04:13 -0300

    divicao da pagina user nas paginas login e register.

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { useState } from "react";
+import { UserLogin } from "../components/user/userLogin";
+import { UserSignIn } from "../components/user/userSignIn";
+
+export default function UserPage(){
+    return(
+        <div>
+        </div>
+    )
+}
---
+++ src/app/login/page/api/login/index.ts
@@ -0,0 +1,7 @@
+import { prisma } from "../../../../../lib/prisma";
+import { authentication } from "@/src/lib/middleware";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export function UserLogin(){
+    
+}
