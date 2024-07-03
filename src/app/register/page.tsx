0e23d84961ad6bbc5a30aca5586bb4e7f9f73338
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-07-03 11:43:17 -0300

    adicao do toastContainer na pagina de registrar o usuario.

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,20 @@
 "use client";
 import { UserSignIn } from "../components/user/userSignIn";
+import {Slide, ToastContainer} from "react-toastify";
 
 export default function Register(){
-    return <UserSignIn/>
+    return (
+        <div className="">
+            <UserSignIn/>
+
+            <ToastContainer
+                hideProgressBar={true}
+                position="top-right"
+                transition={Slide}
+                autoClose={500}
+                closeOnClick
+                pauseOnHover
+            />
+        </div>
+    )
 }
