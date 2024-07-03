d3122a19886a1141706db722c84a550832a5a81a
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-07-03 10:34:36 -0300

    desenvolvimento do middleware.

--- src/lib/middleware.ts
+++ src/lib/middleware.ts
@@ -1,8 +1,22 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
 
-const secretKey = process.env.KEY_JWT;
+const secretKey = process.env.KEY_JWT ?? "";
 
-export function authentication(){
-    
+export function authentication(handler: NextApiHandler){
+    return async (request: NextApiRequest, response: NextApiResponse) => {
+        const token = request.cookies.auth;
+
+        if(!token){
+            response.status(401).json({mensage: "Falha na autenticação"});
+            return;
+        }
+
+        try{
+            jwt.verify(token, secretKey);
+            return handler(request, response);
+        } catch(error){
+            response.status(401).json({mensage: "Falha na autenticação"});
+        }
+    }
 }
